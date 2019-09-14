11dea5fab9ba5683444d32116e6bdf5ffdacead7
Will Comfort <comfortwb21@gmail.com>
2019-09-14 01:09:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,7 +176,8 @@ def player_numbers(team_name)
   game_hash.map do |place, team|
      if team[:team_name] == team_name
        team.each do |attributes, data|
-         if attributes == 
+         if attributes == :name 
+           
 
 
 
