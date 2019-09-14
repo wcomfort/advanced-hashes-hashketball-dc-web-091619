a5b1f075c65c9ce103cf8102df5265deb2df4d90
Will Comfort <comfortwb21@gmail.com>
2019-09-14 01:28:04 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -28,7 +28,7 @@ hash = {
       :slam_dunks => 7
     },
      {
-      :name => "Brock Lopez",
+      :name => "Brook Lopez",
       :number => 11,
       :shoe => 17,
       :points => 17,
@@ -42,7 +42,7 @@ hash = {
       :number => 1,
       :shoe => 19,
       :points => 26,
-      :rebounds => 12,
+      :rebounds => 11,
       :assists => 6,
       :steals => 3,
       :blocks => 8,
@@ -88,7 +88,7 @@ hash = {
       :slam_dunks => 10
     },
      {
-      :name => "DeSanga Diop",
+      :name => "DeSagna Diop",
       :number => 2,
       :shoe => 14,
       :points => 24,
@@ -110,13 +110,13 @@ hash = {
       :slam_dunks => 0
     },
      {
-      :name => "Brendan Haywood",
+      :name => "Kemba Walker",
       :number => 33,
       :shoe => 15,
       :points => 6,
       :rebounds => 12,
       :assists => 12,
-      :steals => 22,
+      :steals => 7,
       :blocks => 5,
       :slam_dunks => 12
    }
@@ -126,23 +126,93 @@ hash = {
 end 
 
 def num_points_scored(name)
+  final_points = nil
 game_hash.each do |place, team|
   team.each do |attribute, data|
     if attribute == :players
     data.each do |player|
       if player[:name] == name
-      return player[:points]
+      final_points = player[:points]
        end 
       end
     end
-  end 
- end 
+  end
+ end
+  #binding.pry
+ final_points
 end
-  
 
+def shoe_size(name)
+  player_shoe = nil 
+  game_hash.each do |place, team|
+  team.each do |attribute, data|
+    if attribute == :players
+    data.each do |player|
+      if player[:name] == name
+      player_shoe = player[:shoe]
+       end 
+      end
+    end
+  end
+ end
+ player_shoe
+end 
 
+def team_colors(team_name)
+  game_hash.each do |place, team|
+    if team[:team_name] == team_name
+  return team[:colors]
+  end 
+  end
+end 
 
+def team_names
+  game_hash.map do |place, team|
+    team[:team_name]
+  end
+end
 
+def player_numbers(team_name)
+  nums = []
+  game_hash.map do |place, team|
+     if team[:team_name] == team_name
+       team.each do |attributes, data|
+         if attributes == :players 
+           data.each do |player|
+         nums <<  player[:number]
+         end
+       end
+     end
+   end
+ end
+ nums 
+ end 
+ 
+def player_stats(player_name)
+  player_stats = {}
+  game_hash.each do |team, data|
+    data[:players].each do |stats|
+   if stats[:name] == player_name
+        stats.delete(:name)
+        player_stats = stats
+      end
+    end
+  end
+  player_stats
+end
 
+def big_shoe_rebounds
+  big_shoes_guy = 0
+  rebounds = 0
+    game_hash.each do | team, data|
+      data[:players].each do | stats |
+        if stats[:shoe] > big_shoes_guy
+          big_shoes_guy = stats[:shoe]
+          rebounds = stats[:rebounds]
+        end
+      end
+    end
+  rebounds
+end
 
 
