181f31cb5f4eec4b1790d172ca32e54ff8823ae6
Will Comfort <comfortwb21@gmail.com>
2019-09-14 01:17:35 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,6 +196,7 @@ def player_numbers(team_name)
          data.each do |player|
            if player[:name] = name
              new_hash = player.delete_if do |k, v|
+               k = :name 
                
 
 
